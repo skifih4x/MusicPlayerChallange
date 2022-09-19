600be8990b331fa29226f24ea7c634212c3fea4c
artem <arte@kb.ri>
2022-09-20 00:24:43 +0300

    Initial Commit

---
+++ MusicPlayerChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayerChallange
+//
+//  Created by Артем Орлов on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
