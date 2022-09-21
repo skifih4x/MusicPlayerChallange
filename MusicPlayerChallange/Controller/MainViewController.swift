bfde2c5894b24b9f50a37af4d833ef12a4312067
Alexander Altman <qewhouse@gmail.com>
2022-09-21 16:25:08 +0300

    player screen hard code

--- MusicPlayerChallange/Controller/MainViewController.swift
+++ MusicPlayerChallange/Controller/MainViewController.swift
@@ -12,21 +12,21 @@ class MainViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
             collectionView.dataSource = self
-            collectionView.register(UINib(nibName: "HeaderView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "HeaderView")
-            collectionView.register(UINib(nibName: "ContentCell", bundle: nil), forCellWithReuseIdentifier: "ContentCell")
+            //            collectionView.register(UINib(nibName: "HeaderView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "HeaderView")
+            //            collectionView.register(UINib(nibName: "ContentCell", bundle: nil), forCellWithReuseIdentifier: "ContentCell")
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.collectionViewLayout = layout
-     
+        registerCells()
     }
-
+    
     let layout: UICollectionViewCompositionalLayout = {
         
         let inset: CGFloat = 2.5
-       
+        
         //Item
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -45,10 +45,6 @@ class MainViewController: UIViewController {
         let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: "header", alignment: .top)
         section.boundarySupplementaryItems = [header]
         
-        // Decoration Item - BACKGROUND
-//        let backgroundItem = NSCollectionLayoutDecorationItem.background(elementKind: "background")
-//        section.decorationItems = [backgroundItem]
-        
         // Section Configuration
         let config = UICollectionViewCompositionalLayoutConfiguration()
         config.interSectionSpacing = 20
@@ -57,7 +53,11 @@ class MainViewController: UIViewController {
         
         return UICollectionViewCompositionalLayout(section: section, configuration: config)
     }()
-
+    // MARK: - Private Methods
+    private func registerCells() {
+        collectionView.register(UINib(nibName: "HeaderView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "HeaderView")
+        collectionView.register(UINib(nibName: "ContentCell", bundle: nil), forCellWithReuseIdentifier: "ContentCell")
+    }
 }
 
 extension MainViewController: UICollectionViewDataSource {
@@ -70,7 +70,7 @@ extension MainViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCell", for: indexPath)
         cell.backgroundColor = .red
         return cell
     }
