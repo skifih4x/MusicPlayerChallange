a1729e5c112d9e1dcb24b0c8149fc43c6a517c46
Alexander Altman <qewhouse@gmail.com>
2022-09-21 15:26:06 +0300

    CollectionView main screen

--- MusicPlayerChallange/Controller/MainViewController.swift
+++ MusicPlayerChallange/Controller/MainViewController.swift
@@ -7,60 +7,75 @@
 
 import UIKit
 
-final class MainViewController: UICollectionViewController {
+class MainViewController: UIViewController {
     
-    // MARK: - Private Properties
-    private let compositionalLayout: UICollectionViewCompositionalLayout = {
-        let inset: CGFloat = 8
-        
-        // Item
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        // Group
-        let outerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .fractionalHeight(0.3))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: outerGroupSize, subitems: [item])
-        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: inset, bottom: 0, trailing: inset)
-        
-        // Section
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
-        section.orthogonalScrollingBehavior = .continuous
-        
-        // Supplementary Item - HEADER
-        let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
-        let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: "header", alignment: .top)
-        headerItem.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: inset, bottom: 8, trailing: inset)
-        section.boundarySupplementaryItems = [headerItem]
-        
-        // Decoration Item - BACKGROUND
-        let backgroundItem = NSCollectionLayoutDecorationItem.background(elementKind: "background")
-        section.decorationItems = [backgroundItem]
-        
-        // Section Configuration
-        let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.interSectionSpacing = 20
-        
-        // Make UICollectionViewCompositionalLayout
-        let layout = UICollectionViewCompositionalLayout(section: section, configuration: config)
-        layout.register(BackgroundDecorationView.self, forDecorationViewOfKind: "background")
-        
-        return layout
-    }()
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.dataSource = self
+            collectionView.register(UINib(nibName: "HeaderView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "HeaderView")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.collectionViewLayout = compositionalLayout
+        collectionView.collectionViewLayout = layout
+     
     }
 
-    
-    // MARK: - Private Methods
-    private func registerCells() {
-        collectionView.register(UINib(nibName: "ContentCell", bundle: nil), forCellWithReuseIdentifier: "ContentCell")
-        collectionView.register(UINib(nibName: "HeaderSupplementaryView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "ContentHeader")
-    }
+    let layout: UICollectionViewCompositionalLayout = {
+        
+        let inset: CGFloat = 2.5
+       
+        //Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        
+        //Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .fractionalHeight(0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        //Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        // Supplementary
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(44))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: "header", alignment: .top)
+        
+        section.boundarySupplementaryItems = [header]
+        
+        
+        
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }()
 
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return section == 2 ? 15 : 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let view = collectionView.dequeueReusableSupplementaryView(ofKind: "header", withReuseIdentifier: "HeaderView", for: indexPath) as? HeaderView else {
+            return UICollectionReusableView()
+        }
+        
+        view.title = indexPath.section == 1 ? "Music to listen" : "Recently viewed"
+        return view
+    }
     
     
     
