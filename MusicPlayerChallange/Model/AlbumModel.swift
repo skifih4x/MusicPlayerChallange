087af3cb4aa033eed40d9e27c669d035c752078e
artem <arte@kb.ri>
2022-09-22 22:02:22 +0300

    add albomModel

---
+++ MusicPlayerChallange/Model/AlbumModel.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumModel.swift
+//  MusicPlayerChallange
+//
+//  Created by Артем Орлов on 22.09.2022.
+//
+
+import Foundation
+
+struct AlbumModel: Decodable {
+    let results: [ResultsAlbum]
+}
+
+struct ResultsAlbum: Decodable {
+    let collectionId: Int // id для перехода к трекам альбома
+    let artistName: String
+    let artworkUrl100: String?
+    let trackCount: Int
+    let collectionName: String
+}
